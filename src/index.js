7db22725db5fc12e4c665ad2f08622ebae50a444
fpoon777 <fpoon777@gmail.com>
2022-04-13 18:09:11 -0400

    fixed starterpack

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
-import SearchBar from './components/search_bar';
-import youtubeSearch from './youtube-api';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-    };
-
-    youtubeSearch('pixar').then((videos) => {
-      this.setState({
-        videos,
-        selectedVideo: videos[0],
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} />
-      </div>
-    );
-  }
+function App() {
+  return <div className="test">All the REACT are belong to us!</div>;
 }
 
 const root = createRoot(document.getElementById('main'));
